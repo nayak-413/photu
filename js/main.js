3232cdecb3e08104702e294d43638b95782e6d5e
Saleel Nayak <anuragh0413@gmail.com>
2025-07-26 07:16:38 +0530

    final edited

--- js/main.js
+++ js/main.js
@@ -36,3 +36,36 @@ window.onscroll = function () {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", function () {
+    document
+      .querySelectorAll(".nav-link")
+      .forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.id;
+      const navLink = document.querySelector(`.nav-link[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        if (navLink) navLink.classList.add("active");
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6, // 60% of section in view
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
